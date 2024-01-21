1bbd32fbbcbe26dc5a6ac46186c7ceced721e892
aatif <aatifdom10@gmail.com>
2024-01-21 20:33:43 +0530

    Added splash screen with app icon and branding signature.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -26,7 +25,6 @@ class MyApp extends StatelessWidget {
       preferDesktop: true,
       builder: (p0) => MaterialApp(
         title: 'Wallet',
-        locale: DevicePreview.locale(context),
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
